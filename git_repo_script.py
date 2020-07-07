f59eddc56d85a043ea633d3b3c6ca3ccbd590461
Fraser Carter <fraserc97@hotmail.co.uk>
2020-07-07 20:41:45 +0100

    script now asks for repo name, formats and validates it

--- git_repo_script.py
+++ git_repo_script.py
@@ -2,19 +2,44 @@ import github
 import getpass
 
 def loginToGithub():
-    print("Github login")
-    token = getpass.getpass("Access Token")
-    try:
+    print("GITHUB LOGIN")
+    token = getpass.getpass("Access Token: ") #input token without echo to console
+    try: #test login is valid
         g = github.Github(token)
         testLogin = g.get_user().login
         print(testLogin)
-
-        createGithubRepo()
-        
     except:
-        print("Invalid access token")
+        print("Invalid Access Token")
+    else:
+        createGithubRepo(g)
 
-def createGithubRepo():
-    pass
+def createGithubRepo(login):
+    repoName = input("Input new repository name: ")
+    validName = True
+    if " " in repoName: #format repo name for github
+        print("there is white space")
+        repoName = repoName.replace(" ", "-")
 
-loginToGithub()
+    for repo in login.get_user().get_repos(): #check repo name is valid
+        print(repo.name)
+        if repoName == repo.name:
+            validName = False
+            break;
+            
+    if validName is True: # try to init the repo
+        print("this name has not been used before")
+
+        try:
+            pass
+        except:
+            pass
+    else: #ask user to enter a new name
+        print("repository name already exists, please try again")
+        createGithubRepo(login)
+                
+            
+    #init new repo if name is valid
+
+#loginToGithub()
+g = github.Github("")
+createGithubRepo(g)
